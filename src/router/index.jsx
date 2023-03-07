12cd8c5586be1d2de5307adf0bb5e100a91d4a62
sugiartofahmi <sugiartofahmi@gmail.com>
2023-03-07 22:10:55 +0700

    Fahmi Sugiarto | Restructure Project

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 const Home = lazy(() => import("../views/Home"));
-
+const NotFound = lazy(() => import("../views/NotFound"));
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/skills" element={<Skills />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
